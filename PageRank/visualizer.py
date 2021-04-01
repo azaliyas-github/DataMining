0684d56e9e854d20cf3f45980a92eafbf71b4d2d
azaliyas-github <1maxovmax1@gmail.com>
2021-04-01 22:48:29 +0300

    Пересела на Cytospace для отрисовки графов

--- PageRank/visualizer.py
+++ PageRank/visualizer.py
@@ -1,14 +1,17 @@
 import logging
-from typing import List, Set
+from typing import Dict, List, Set, Tuple
 from urllib.parse import ParseResult, urlparse
 
-from graphviz import Digraph
+from py2cytoscape.data.cynetwork import CyNetwork
+from py2cytoscape.data.cyrest_client import CyRestClient
 
 from implementation.common import page_link_repository_name
 from implementation.infrastructure import configure_logging
 from implementation.page_link import PageLink, PageLinkRepository
 
 log: logging.Logger = logging.getLogger()
+
+layout_name = "circular"
 
 
 def main() -> None:
@@ -38,30 +41,35 @@ def preprocess_url(url: str) -> str:
     scheme: str = parsed_url.scheme + "://"
     url = parsed_url.geturl().replace(scheme, "", 1)
 
-    url = url.replace(":", "")
-
     return url
 
 
 def draw_graph(page_links: List[PageLink]) -> None:
-    graph: Digraph = Digraph()
-    created_nodes: Set[str] = set()  # множество URL, для которых уже созданы узлы графа
-
-    def create_node(url: str) -> None:
-        graph.node(url, url)
-        created_nodes.add(url)
-
     log.info(f"Создаю граф из {len(page_links)} ссылок")
+    graph_nodes: Set[str] = set()
+    graph_edges: List[Tuple[str, str]] = []
     for link in page_links:
-        if link.from_url not in created_nodes:
-            create_node(link.from_url)
-        if link.to_url not in created_nodes:
-            create_node(link.to_url)
+        if link.from_url not in graph_nodes:
+            graph_nodes.add(link.from_url)
+        if link.to_url not in graph_nodes:
+            graph_nodes.add(link.to_url)
 
-        graph.edge(link.from_url, link.to_url)
+        graph_edges.append((link.from_url, link.to_url))
 
-    log.info("Рисую граф ссылок")
-    graph.view()
+    log.info("Рисую граф ссылок с расположением " + layout_name)
+    cytospace_client: CyRestClient = CyRestClient()
+    graph: CyNetwork = cytospace_client.network.create()
+
+    node_suids: Dict[str, int] = graph.add_nodes(list(graph_nodes))
+    graph.add_edges(list(
+        {"source": node_suids[edge[0]], "target": node_suids[edge[1]]}
+        for edge in graph_edges))
+    cytospace_client.layout.apply(layout_name, graph)
+
+    image_height: int = max(len(graph_nodes) * 60, 600)
+    graph_image: bytes = graph.get_png(image_height)
+    with open(f"links-{layout_name}.png", "wb+") as png_file:
+        png_file.write(graph_image)
 
 
 if __name__ == '__main__':
