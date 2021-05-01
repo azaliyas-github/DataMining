fc42e478c752e96e2b8f92543e4f60b282b186b6
azaliyas-github <1maxovmax1@gmail.com>
2021-05-01 19:23:08 +0500

    Сохраняем матрицу переходов в файл

--- PageRank/visualizer.py
+++ PageRank/visualizer.py
@@ -1,8 +1,10 @@
 import logging
 import sys
+from itertools import chain
 from typing import Dict, List, Set, Tuple
 from urllib.parse import ParseResult, urlparse
 
+from pandas import DataFrame
 from py2cytoscape.data.cynetwork import CyNetwork
 from py2cytoscape.data.cyrest_client import CyRestClient
 
@@ -20,6 +22,7 @@ def main() -> None:
 
     page_links: List[PageLink] = get_page_links(page_link_repository_name)
 
+    output_transition_matrix(page_links)
     draw_graph(page_links, layout_name)
 
 
@@ -79,6 +82,42 @@ def draw_graph(page_links: List[PageLink], layout_name: str) -> None:
     graph_image: bytes = graph.get_png(image_height)
     with open(f"links-{layout_name}.png", "wb+") as png_file:
         png_file.write(graph_image)
+
+
+def output_transition_matrix(page_links: List[PageLink]):
+    page_urls: List[str] = list(
+        chain(
+            (link.from_url for link in page_links),
+            (link.to_url for link in page_links)))
+    index: Dict[str, int] = dict()
+    for url in page_urls:
+        if url not in index:
+            index[url] = len(index)
+
+    log.info("Строю индекс")
+    index_csv: DataFrame = DataFrame(data = {"url": index.keys()}, index = index.values())
+
+    log.info("Сохраняю индекс")
+    index_csv.to_csv("index.csv", index_label = "id")
+    del index_csv
+
+    log.info("Строю матрицу переходов")
+    empty_column: List[float] = [0.0] * len(page_urls)
+    matrix: DataFrame = DataFrame(
+        data = dict((id_, empty_column) for id_ in index.values()),
+        dtype = float)
+    for link in page_links:
+        from_url_index: int = index[link.from_url]
+        to_url_index: int = index[link.to_url]
+        matrix.at[to_url_index, from_url_index] = float(link.count)
+    for column in matrix.columns:
+        column_sum: float = matrix[column].sum()
+        if column_sum > 0:
+            matrix[column] = matrix[column] / column_sum
+
+    log.info("Сохраняю матрицу переходов")
+    matrix.to_csv("matrix.csv")
+    del matrix
 
 
 if __name__ == '__main__':
